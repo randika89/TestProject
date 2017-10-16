4787783f96ba242e52f7db2d1df94a8292a1fb1c
Randika89 <randika1989@yahoo.com>
2017-10-16 06:11:07 +0530

    commit

--- app/src/main/java/com/example/randika/testproject/MainActivity.java
+++ app/src/main/java/com/example/randika/testproject/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.randika.testproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera camera;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    private Camera.Parameters params;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +35,45 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Snackbar timer_snackbar = Snackbar.make(view,"Start Auto Off timer for 1 hr", Snackbar.LENGTH_LONG);
+                timer_snackbar.setAction("Start", new TimerListener());
+                timer_snackbar.show();
+            }
+        });
+
+       hasFlash = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (!hasFlash) {
+            // device doesn't support flash
+            // Show alert message and close the application
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("Sorry, your device doesn't support flash light!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
+
+        Switch onOffSwitch = (Switch)  findViewById(R.id.switch1);
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //Log.v("Switch On", "Switch On");
+                    getCamera();
+                    turnOnFlash();
+                }else{
+                    //Log.v("Switch Off", "Switch Off");
+                    turnOffFlash();
+                }
             }
         });
     }
@@ -49,4 +99,78 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // getting camera parameters
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                params = camera.getParameters();
+            } catch (RuntimeException e) {
+                //Log.e("Camera Open Error", e.getMessage());
+            }
+        }
+    }
+
+    /*
+* Turning On flash
+*/
+    private void turnOnFlash() {
+        if (!isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+            isFlashOn = true;
+
+            // changing button/switch image
+            //toggleButtonImage();
+        }
+    }
+
+        /*
+        * Turning Off flash
+        */
+        private void turnOffFlash() {
+            if (isFlashOn) {
+                if (camera == null || params == null) {
+                    return;
+                }
+
+                params = camera.getParameters();
+                params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                camera.setParameters(params);
+                camera.stopPreview();
+                isFlashOn = false;
+
+                // changing button/switch image
+                //toggleButtonImage();
+            }
+        }
+
+    private class TimerListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Log.v("MyUndoListener", "MyUndoListener");
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("Sorry, your device doesn't support flash light!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+            // Code to undo the user's last action
+        }
+    }
 }
+
+
